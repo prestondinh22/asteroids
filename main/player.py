4fce9b8d48b371dc6d1dd2fb4058ce6aff60da24
prestondinh22 <prestodinh11@gmail.com>
2025-05-09 05:16:58 +0000

    added movement to sprite

--- main/player.py
+++ main/player.py
@@ -19,4 +19,28 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
- 
+    
+    def rotate(self, dt):
+        player_rotate_speed = PLAYER_TURN_SPEED * dt
+        self.rotation += player_rotate_speed
+        return self.rotation
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate((-dt))
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_s]:
+            self.backward(dt)
+        if keys[pygame.K_w]:
+            self.forward(dt)
+    
+    def forward(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def backward(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += -forward * PLAYER_SPEED * dt
